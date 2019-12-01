96c467ac35263dc6bda4e05d14bf795645c81a52
CharityParks <tuppercharity@gmail.com>
2019-12-01 17:14:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Charity "
+    end
+
+    get '/hometown' do
+        "My hometown is Liberty Twp"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Fast Cars and Freedom"
+    end
+
 end
+#"My name is __"
