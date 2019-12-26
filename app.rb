fe60361a4cd76cd484c5ff6581a35af9f491d320
Amgad Malik <amgadmalik@yahoo.com>
2019-12-26 02:18:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
+
+  get '/name' do
+    @name = "My name is __"
+    # erb :'name/index.html.erb'
+  end
+  
+  get '/hometown' do
+    @hometown = "My hometown is __"
+    # erb :'hometown/index.html.erb'
+  end
+
+  get '/favorite-song' do
+    @favorite_song = "My favorite song is __"
+    # erb :'favorite_song/index.html.erb'
+  end
+  
 end
